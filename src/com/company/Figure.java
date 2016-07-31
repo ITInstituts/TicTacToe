09cf0373ea1a38511e31b7a782f8314761d17d12
Maxim <astratenko.m@gmail.com>
2016-07-31 22:17:52 +0300

    Project update

--- src/com/company/Figure.java
+++ src/com/company/Figure.java
@@ -4,4 +4,7 @@ package com.company;
  * Created by Maxim on 25.07.2016.
  */
 public class Figure {
+    private String x = "\u274C";
+    private String o = "\u25EF";
+    private String nullFigure = "\u25A0";
 }
